3f1b2f69a6b498b7b0c81ad4977abd409cb13ab5
hungvuduy1997 <hungvuduy1997@gmail.com>
2026-07-08 13:30:25 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -133,7 +133,7 @@ async function rebuildLayers(theme) {
     const val = combinedData[activeTheme.attribute];
     const def = activeTheme.categories[val];
     // Example: Muted dark charcoal for light mode, and a bright soft silver for dark mode
-    const fallbackColor = currentMode === 'dark' ? '#b0bec5' : '#7f7f7f';
+    const fallbackColor = currentMode === 'dark' ? '#4c4c4c' : '#7f7f7f';
     const color = def ? (currentMode === 'dark' ? def.darkColor : def.lightColor) : fallbackColor;
     
     const metersWidth = getHighwayWidthInMeters(hw);
